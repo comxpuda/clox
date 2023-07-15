f6b7f6f5901e96718afa1e84a54e98abb7d543c3
comxpuda <comxpuda@gmail.com>
2023-07-15 15:53:27 +0800

    global variables

--- vm.c
+++ vm.c
@@ -34,11 +34,13 @@ void initVM()
     resetStack();
     vm.objects = NULL;
     iniTable(&vm.strings);
+    iniTable(&vm.globals);
 }
 
 void freeVM()
 {
   freeTable(&vm.strings);
+  freeTable(&vm.globals);
   freeObjects();
 }
 
@@ -82,6 +84,7 @@ static InterpretResult run()
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_CONSTANT_LONG() (vm.chunk->constants.values[READ_BYTE() << 8 | READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
     do { \
       if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
@@ -123,6 +126,32 @@ static InterpretResult run()
             case OP_NIL: push(NIL_VAL); break;
             case OP_TRUE: push(BOOL_VAL(true)); break;
             case OP_FALSE: push(BOOL_VAL(false)); break;
+            case OP_POP:  pop(); break;
+            case OP_GET_GLOBAL: {
+              ObjString* name = READ_STRING();
+              Value value;
+              if (!tableGet(&vm.globals,name,&value)) {
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+              }
+              push(value);
+              break;
+            }
+            case OP_DEFINE_GLOBAL: {
+              ObjString* name = READ_STRING();
+              tableSet(&vm.globals, name, peek(0));
+              pop();
+              break;
+            }
+            case OP_SET_GLOBAL: {
+              ObjString* name = READ_STRING();
+              if (tableSet(&vm.globals, name, peek(0))) {
+                tableDelete(&vm.globals, name); 
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+              }
+              break;
+            }
             case OP_EQUAL: {
               Value b = pop();
               Value a = pop();
@@ -168,16 +197,24 @@ static InterpretResult run()
                 push(NUMBER_VAL(-AS_NUMBER(pop())));
                 break;
             }
+            case OP_PRINT: {
+              printValue(pop());
+              printf("\n");
+              break;
+            }
             case OP_RETURN: {
-                printValue(pop());
-                printf("\n");
+                // printValue(pop());
+                // printf("\n");
                 return INTERPRET_OK;
             }
         }
     }
 
-#undef READ_BYTE
-#undef READ_CONSTANT
+#undef  READ_BYTE
+#undef  READ_CONSTANT
+#undef  READ_CONSTANT_LONG
+#undef  READ_STRING
+#undef  BINARY_OP
 }
 
 InterpretResult interpret(const char* source) {
